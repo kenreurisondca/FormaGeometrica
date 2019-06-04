97eecc06f5b1b1cfa6691051d3fe445400e63bd6
kenreurisondca <kenreurison@dca.ufrn.br>
2019-06-04 17:58:15 -0300

    Create Node.cpp

---
+++ Listas/Node.cpp
@@ -0,0 +1,35 @@
+#ifndef NODE_CPP
+#define NODE_CPP
+class Node {
+	private:
+		int info;
+		Node* prox;
+	public:
+		Node();
+		Node(int);
+		Node(Node*);
+		Node* getProx();
+		int getInfo();
+		void setProx(Node*);
+};
+Node::Node(){}
+Node::Node(int value){
+	info = value;
+	prox = 0;
+}
+int Node::getInfo(){
+	return info;
+}
+Node::Node(Node *n){
+	info = n->info;
+	prox = n->prox;
+}
+Node* Node::getProx(){
+	return prox;
+}
+
+void Node::setProx(Node *n){
+	prox = n;
+}
+#endif
+
